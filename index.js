cde552d5e6486015f9c75c77e476c1b60a6a4d04
akhilesh-code-96 <srivastavakhilesh157@gmail.com>
2024-06-22 21:51:14 +0530

    Initiliazing App

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = 3000;
+const server = express();
+
+
+server.get("/", (req, res) => {
+  return res.send("Welcome to Inventory App");
+})
+
+server.listen(PORT, () => {
+  console.log(`running at http://localhost:${PORT}`);
+})
