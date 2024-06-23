eab0131a16f5f9c3d55b940cb1212f575958628c
akhilesh-code-96 <srivastavakhilesh157@gmail.com>
2024-06-23 20:54:11 +0530

    Completed the data validation for the add new product view

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -8,10 +8,29 @@ export default class ProductController {
   }
 
   getAddForm(req, res) {
-    return res.render("new-product");
+    return res.render("new-product", {errorMessage: null});
   }
 
   addNewProduct(req, res) {
+    // validate data.
+    const { name, price, imageUrl } = req.body;
+    let errors = [];
+    if (!name || name.trim() == "") {
+      errors.push("Name is required");
+    }
+    if (!price || parseFloat(price) < 1) {
+      errors.push("Price must be a positive value");
+    }
+    try {
+      const validUrl = new URL(imageUrl);
+    } catch (error) {
+      errors.push("Url is invalid");
+    }
+
+    if (errors.length > 0) {
+      return res.render("new-product", { errorMessage: errors[0] });
+    }
+
     ProductModel.add(req.body);
     let products = ProductModel.get();
     return res.render("products", { products });
