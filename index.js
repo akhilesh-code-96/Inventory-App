d21d3774affdbd628027158a6cd5a92bd3c7cc44
akhilesh-code-96 <srivastavakhilesh157@gmail.com>
2024-06-25 21:08:44 +0530

    changed the css a bit and implemented registration and login page along with authentication.

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import expressEjsLayouts from "express-ejs-layouts";
 import path from "path";
 import validateRequest from "./src/middlewares/validation.middleware.js";
 import { uploadFile } from "./src/middlewares/file-upload.middleware.js";
+import UserController from "./src/controllers/user.controller.js";
 
 const PORT = 3000;
 const server = express();
 
-server.use(express.static('public'));
+server.use(express.static("public"));
 
 // parse form data.
 server.use(express.urlencoded({ extended: true }));
@@ -19,6 +20,14 @@ server.set("views", path.join(path.resolve(), "src", "views"));
 
 server.use(expressEjsLayouts);
 
+// create an instance of userController model.
+const userController = new UserController();
+// setting up routes for the userController model.
+server.get("/register", userController.getRegister);
+server.get("/login", userController.getLogin);
+server.post("/register", userController.postRegister);
+server.post("/login", userController.postLogin);
+
 //create an instance of product controller.
 const productController = new ProductController();
 //creating routes for all the functionalities.
@@ -28,7 +37,12 @@ server.get("/update-product/:id", productController.getUpdateProductView);
 server.post("/", productController.postUpdateProductResponse);
 server.post("/delete-product/:id", productController.deleteProduct);
 // adding middleware specific to this method.
-server.post("/upload", uploadFile.single("imageUrl"), validateRequest, productController.postAddProduct);
+server.post(
+  "/upload",
+  uploadFile.single("imageUrl"),
+  validateRequest,
+  productController.postAddProduct
+);
 
 //Middleware.
 server.use(express.static("src/views"));
